5f7cf4649294eb59aa18e6b6e85e6fbb5fe9c531
Mauli Zambare <168577916+MauliZambare@users.noreply.github.com>
2025-07-27 01:25:47 +0530

    Delete bankapp directory
    Wrong Commit

--- bankapp/src/main/java/com/bank/bankapp/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.bankapp.repository;
-
-import com.bank.bankapp.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
